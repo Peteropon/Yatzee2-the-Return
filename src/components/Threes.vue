e798253ddd6c5477ae69f11b1947bf306dbd6040
Petros Tsavdaridis <peteropon@gmail.com>
2019-06-13 23:26:21 +0200

    Added new component for threes

---
+++ src/components/Threes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <span>Threes: {{ getThrees }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Threes',
+    computed: {
+        getThrees() {
+            return this.$store.state.threes * 3
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/UpperTable.vue
+++ src/components/UpperTable.vue
@@ -2,17 +2,20 @@
     <div>
         <ones></ones>
         <twos></twos>
+        <threes></threes>
     </div>
 </template>
 
 <script>
 import Ones from './Ones'
 import Twos from './Twos'
+import Threes from './Threes'
 export default {
     name: 'UpperTable',
     components: {
         Ones,
         Twos,
+        Threes,
     },
 }
 </script>
