1a8e0666e3e55be80f7a455228010c133184e671
Petros Tsavdaridis <peteropon@gmail.com>
2019-06-14 23:27:03 +0200

    Added a finalSum variable to display the sum after a component has been selected

--- src/components/Fours.vue
+++ src/components/Fours.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-        <span>Fours: {{ getFours }}</span>
+    <div @click="registerPoints">Fours:
+        <span v-if="!selected">{{ getFours }}</span>
+        <span v-else>{{ finalSum }}</span>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Fours',
+    data() {
+        return {
+            selected: false,
+            finalSum: 0
+        }
+    },
     computed: {
         getFours() {
             return this.$store.state.fours * 4
+        },
+    },
+    methods: {
+        registerPoints() {
+            this.selected = true
+            this.$store.dispatch('registerPoints')
+            this.finalSum = this.$store.state.fours * 4
         }
     }
+
 }
 </script>
 
--- src/components/Threes.vue
+++ src/components/Threes.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        <span>Threes: {{ getThrees }}</span>
+    <div @click="registerPoints"> Threes:
+        <span v-if="!selected"> {{ getThrees }}</span>
+        <span v-else>{{ finalSum }}</span>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Threes',
+    data() {
+        return {
+            selected: false,
+            finalSum: 0
+        }
+    },
     computed: {
         getThrees() {
             return this.$store.state.threes * 3
+        },
+    },
+    methods: {
+        registerPoints() {
+            this.selected = true
+            this.$store.dispatch('registerPoints')
+            this.finalSum = this.$store.state.threes * 3
         }
     }
 }
